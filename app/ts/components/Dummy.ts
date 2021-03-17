6b9e0e548680853dd9cf2bc63fc6e463ced27b84
jtornert <joakim.tornert@outlook.com>
2021-03-17 13:27:41 +0100

    changed dummy component style for clarity

--- app/ts/components/Dummy.ts
+++ app/ts/components/Dummy.ts
@@ -1,6 +1,8 @@
-export default function (text) {
+export default function (text: string) {
   const dummy = document.createElement('p');
   dummy.classList.add('dummy');
+  dummy.style.backgroundColor = 'gainsboro';
+  dummy.style.padding = '1rem';
   dummy.innerText = text;
 
   return dummy;
--- app/ts/index.ts
+++ app/ts/index.ts
@@ -1,6 +1,6 @@
 import Dummy from './components/Dummy';
 
-const dummy = Dummy('Template compontent');
+const dummy = Dummy('Dummy component');
 document.body.appendChild(dummy);
 
 console.log('Fullstack template v1');

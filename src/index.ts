2e2904a49682bc0be39b93e0fc9c6e57c61bb426
jtornert <joakim.tornert@outlook.com>
2021-03-17 13:36:32 +0100

    renamed import of ./utils/paths to paths

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
-import * as utils from './utils/paths';
+import * as paths from './utils/paths';
 
 import indexRouter from './controllers/index.router';
 
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT ?? 80;
 
-app.use(express.static(utils.root)); // serve static files like .html and .css
+app.use(express.static(paths.root)); // serve static files like .html and .css
 app.use(express.json()); // accept requests with json body
 app.use(express.urlencoded({ extended: true })); // accept html form submissions
 
